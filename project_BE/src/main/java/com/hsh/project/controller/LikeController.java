1096f3bbe0ac369b50cd92f16ccd8a2b5fd2c8d2
Trung0405 <169061111+Trung0405@users.noreply.github.com>
2025-06-21 13:39:55 +0700

    Update LikeController.java

--- project_BE/src/main/java/com/hsh/project/controller/LikeController.java
+++ project_BE/src/main/java/com/hsh/project/controller/LikeController.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import com.hsh.project.security.CustomAccountDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;

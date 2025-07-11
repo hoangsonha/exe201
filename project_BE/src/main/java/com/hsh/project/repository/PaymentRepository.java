52423b90f2394762564e2efa1a9da61584702159
Trung0405 <trunghqse171495@fpt.edu.vn>
2025-07-11 22:15:37 +0700

    adjust Payment func (almost done)

--- project_BE/src/main/java/com/hsh/project/repository/PaymentRepository.java
+++ project_BE/src/main/java/com/hsh/project/repository/PaymentRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByUserOrderByCreatedAtDesc(User user);
+
+    Optional<Payment> findByUser_UserIdAndTransactionId(Long userId, String transactionId);
+
+    Optional<Payment> findByTransactionId(String transactionId);
 }
